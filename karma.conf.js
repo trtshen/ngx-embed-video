6a341f582ab057cbf91a82bff0e1bd01e3d80b56
chaw <3895281+trtshen@users.noreply.github.com>
2022-01-20 16:07:56 +0800

    not working

--- karma.conf.js
+++ karma.conf.js
@@ -1,89 +1,40 @@
-// Karma configuration for Unit testing
-const path = require('path');
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = (config) => {
+module.exports = function (config) {
   config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    entry: () => './src',
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      'karma-jasmine',
-      'karma-chrome-launcher',
-      'karma-webpack',
-      'karma-sourcemap-loader',
-      'karma-spec-reporter',
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-
-    // list of files / patterns to load in the browser
     files: [
-      { pattern: 'spec.bundle.js', watched: false }
+      { pattern: 'tests/**.spec.ts', watched: false }
     ],
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'spec.bundle.js': ['webpack']
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-
-    // webpack
-    resolve: {
-      extensions: ['.ts']
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage'),
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        statements: 80,
+        lines: 80,
+        branches: 70,
+        functions: 80
+      }
     },
-    module: {
-      rules: [
-        {
-          test: /\.ts/,
-          loaders: ['ts-loader'],
-          exclude: /node_modules/
-        }
-      ],
-      exprContextCritical: false
-    },
-    performance: { hints: false },
-
-    webpackServer: {
-      noInfo: true
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
-
-
-    // web server port
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    singleRun: false,
   });
-}
+};
